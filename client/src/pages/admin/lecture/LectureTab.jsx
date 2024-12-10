1870f90544cbc7bdafbd184d75329a3f5b3e7dce
Aditya Thakur <121376593+AdityaThakur79@users.noreply.github.com>
2024-12-10 12:08:23 +0530

    Update LectureTab.jsx

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8080/api/media";
+const MEDIA_API = "https://lms-main-hpi3.onrender.com/api/media";
 
 const LectureTab = () => {
     const [lectureTitle, setLectureTitle] = useState("");
